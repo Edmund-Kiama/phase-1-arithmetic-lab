4622b015c8c688f10aa216354563cd264024b91d
Edmund <edmundgachanja@gmail.com>
2024-11-26 12:49:08 +0300

    Creates arithmeic variables in index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+//setting up numerical variables
+let num1 = 1;
+let num2 = 62;
+let num3 = 24;
+let num4 = 5;
+let set = (1,4,12,15,2,20);
+
+
+//arithmetic variables
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()) + 1;
+const mod = num3 % num4;
+const max = Math.max(set);
